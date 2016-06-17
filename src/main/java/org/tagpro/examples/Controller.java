5c5b8aa59ff504db8fa2c8cb622b041be0fb2b4c
Christian <christian1195@gmail.com>
2016-06-17 15:46:08 +0200

    Goto added to Controller.java

--- src/main/java/org/tagpro/examples/Controller.java
+++ src/main/java/org/tagpro/examples/Controller.java
@@ -5,8 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.tagpro.tasc.Command;
 import org.tagpro.tasc.ConcurrentSetKeyState;
 import org.tagpro.tasc.KeyState;
+import org.tagpro.tasc.data.BallUpdate;
 import org.tagpro.tasc.data.Key;
 import org.tagpro.tasc.data.KeyAction;
+import org.tagpro.tasc.data.Tile;
+
+import java.awt.geom.Point2D;
 
 public class Controller implements Command.KeyObserver {
     private final Logger log = LoggerFactory.getLogger(getClass());
@@ -50,5 +54,51 @@ public class Controller implements Command.KeyObserver {
 
     public KeyState getKeyState() {
         return keyState;
+    }
+
+    public boolean goTo(Tile tile, BallUpdate update) {
+        if (tile == null) {
+            stop();
+            return false;
+        }
+        Point2D.Float distance = goTo(tile.getX(), tile.getY(), update);
+        return Math.abs(distance.x) < 0.5f && Math.abs(distance.y) < 0.5f;
+    }
+
+    public void stop() {
+        for (Key key : Key.values()) {
+            key(key, KeyAction.KEYUP);
+        }
+    }
+
+    public Point2D.Float goTo(float destinationX, float destinationY, BallUpdate update) {
+        float distanceX = destinationX - update.getRx();
+        float distanceY = destinationY - update.getRy();
+
+        log.info("Setting distance X:" + distanceX + " Y:" + distanceY + " " + update);
+
+        if (Math.abs(distanceX) > Math.abs(distanceY)) {
+            //horizontal primary
+            Key primary = distanceX > 0 ? Key.RIGHT : Key.LEFT;
+            key(primary, KeyAction.KEYDOWN);
+
+            //vertical secondary
+            float desiredLy = (distanceY / Math.abs(distanceX)) * Math.abs(update.getLx());
+            Key secondary = desiredLy > 0 ? Key.DOWN : Key.UP;
+            KeyAction action = desiredLy > update.getLy() ? KeyAction.KEYUP : KeyAction.KEYDOWN;
+            key(secondary, action);
+        } else {
+            //vertical primary
+            Key primary = distanceY > 0 ? Key.DOWN : Key.UP;
+            key(primary, KeyAction.KEYDOWN);
+
+            //horizontal secondary
+            float desiredLx = (distanceX / Math.abs(distanceY)) * Math.abs(update.getLy());
+            Key secondary = desiredLx > 0 ? Key.RIGHT : Key.LEFT;
+            KeyAction action = desiredLx > update.getLx() ? KeyAction.KEYUP : KeyAction.KEYDOWN;
+            key(secondary, action);
+        }
+
+        return new Point2D.Float(distanceX, distanceY);
     }
 }

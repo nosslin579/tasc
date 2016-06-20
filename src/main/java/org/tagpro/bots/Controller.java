d61ea98b9ea45f88d5eb2301790dd79080b718ae
Christian <christian1195@gmail.com>
2016-06-20 18:31:57 +0200

    Controller refactor

--- src/main/java/org/tagpro/bots/Controller.java
+++ src/main/java/org/tagpro/bots/Controller.java
@@ -12,7 +12,8 @@ import org.tagpro.tasc.data.Tile;
 import org.tagpro.tasc.extras.ServerStepEstimator;
 import org.tagpro.tasc.starter.Starter;
 
-import java.awt.geom.Point2D;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Controller implements Command.KeyObserver, ServerStepEstimator.ServerStepObserver {
     private final Logger log = LoggerFactory.getLogger(getClass());
@@ -62,22 +63,34 @@ public class Controller implements Command.KeyObserver, ServerStepEstimator.Serv
         return keyState;
     }
 
-    public boolean goTo(Tile tile, BallUpdate update) {
+    public void goTo(Tile tile, BallUpdate update) {
         if (tile == null) {
             stop();
-            return false;
+            return;
         }
-        Point2D.Float distance = goTo(tile.getX(), tile.getY(), update);
-        return Math.abs(distance.x) < 0.5f && Math.abs(distance.y) < 0.5f;
+        goTo(tile.getX(), tile.getY(), update);
     }
 
     public void stop() {
+        log.info("Stopping");
         for (Key key : Key.values()) {
             key(key, KeyAction.KEYUP);
         }
     }
 
-    public Point2D.Float goTo(float destinationX, float destinationY, BallUpdate update) {
+    public void goTo(float destinationX, float destinationY, BallUpdate update) {
+        Map<Key, KeyAction> keysForDirection = getKeysForDirection(destinationX, destinationY, update);
+        setKeys(keysForDirection);
+    }
+
+    private void setKeys(Map<Key, KeyAction> keysForDirection) {
+        for (Map.Entry<Key, KeyAction> entry : keysForDirection.entrySet()) {
+            key(entry.getKey(), entry.getValue());
+        }
+    }
+
+    protected Map<Key, KeyAction> getKeysForDirection(float destinationX, float destinationY, BallUpdate update) {
+        Map<Key, KeyAction> ret = new HashMap<>();
         float distanceX = destinationX - update.getRx();
         float distanceY = destinationY - update.getRy();
 
@@ -86,7 +99,7 @@ public class Controller implements Command.KeyObserver, ServerStepEstimator.Serv
         if (Math.abs(distanceX) > Math.abs(distanceY)) {
             //horizontal primary
             Key primary = distanceX > 0 ? Key.RIGHT : Key.LEFT;
-            key(primary, KeyAction.KEYDOWN);
+            ret.put(primary, KeyAction.KEYDOWN);
 
             //vertical secondary
             float desiredLy = (distanceY / Math.abs(distanceX)) * Math.abs(update.getLx());
@@ -98,11 +111,11 @@ public class Controller implements Command.KeyObserver, ServerStepEstimator.Serv
 
             Key secondary = shouldGoDown ? Key.DOWN : Key.UP;
             KeyAction action = tooFast ? KeyAction.KEYUP : KeyAction.KEYDOWN;
-            key(secondary, action);
+            ret.put(secondary, action);
         } else {
             //vertical primary
             Key primary = distanceY > 0 ? Key.DOWN : Key.UP;
-            key(primary, KeyAction.KEYDOWN);
+            ret.put(primary, KeyAction.KEYDOWN);
 
             //horizontal secondary
             float desiredLx = (distanceX / Math.abs(distanceY)) * Math.abs(update.getLy());
@@ -115,12 +128,9 @@ public class Controller implements Command.KeyObserver, ServerStepEstimator.Serv
 
             Key secondary = shouldGoRight ? Key.RIGHT : Key.LEFT;
             KeyAction action = tooFast ? KeyAction.KEYUP : KeyAction.KEYDOWN;
-            key(secondary, action);
-
-
+            ret.put(secondary, action);
         }
-
-        return new Point2D.Float(distanceX, distanceY);
+        return ret;
     }
 
     @Override
